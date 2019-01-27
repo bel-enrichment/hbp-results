2a4632f55db9d6441dd53ec5dae8cf20f81200d4
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-01-27 20:43:29 +0100

    Update main script

--- main.py
+++ main.py
@@ -4,12 +4,19 @@
 
 import os
 import sys
+from collections import Counter, defaultdict
+from itertools import chain
+from typing import Mapping
 
 import click
+import pandas as pd
 
+import neurommsig_knowledge
 from bel_enrichment import BELSheetsRepository
 from pybel import BELGraph
 from pybel.cli import echo_warnings_via_pager
+from pybel.constants import ANNOTATIONS
+from pybel.struct.grouping import get_subgraphs_by_annotation
 
 AUTHORS = [
     'Charles Tapley Hoyt',
@@ -47,7 +54,7 @@ sheets_repository = BELSheetsRepository(
 )
 
 
-def get_sheets_graph(use_cached: bool = False, use_tqdm: bool = True) -> BELGraph:
+def get_sheets_graph(use_cached: bool = True, use_tqdm: bool = True) -> BELGraph:
     """Get the BEL graph from all Google sheets.
 
     .. warning:: this BEL graph isn't pre-filled with namespace and annotation URLs
@@ -56,17 +63,62 @@ def get_sheets_graph(use_cached: bool = False, use_tqdm: bool = True) -> BELGrap
 
 
 @click.command()
-def main():
-    sheets_repository.generate_curation_summary()
+@click.option('-w', '--show-warnings', is_flag=True)
+def main(show_warnings: bool):
+    # sheets_repository.generate_curation_summary()
 
     graph = get_sheets_graph()
     graph.summarize()
 
+    # summarize API
+    indra_api_histogram = Counter(
+        api
+        for _, _, d in graph.edges(data=True)
+        if ANNOTATIONS in d and 'INDRA_API' in d[ANNOTATIONS]
+        for api in d[ANNOTATIONS]['INDRA_API']
+        if api and isinstance(api, str) and api != 'nan'
+    )
+    click.echo('Readers Used:')
+    api_size = max(len(api) for api in indra_api_histogram)
+    for api, count in indra_api_histogram.most_common():
+        click.echo(f'  {api:{api_size}}: {count}')
+    indra_api_df = pd.DataFrame.from_dict(indra_api_histogram, orient='index')
+    indra_api_df.to_csv(os.path.join(sheets_repository.output_directory, 'api_summary.tsv'), sep='\t')
+
     if graph.warnings:
         number_errored_documents = len({path for path, _, _ in graph.warnings})
-        click.secho(f'Graph had {graph.number_of_warnings()} warnings in {number_errored_documents} documents')
-        echo_warnings_via_pager(graph.warnings)
-        sys.exit(-1)
+        click.secho(f'Warnings from {number_errored_documents} documents')
+        if show_warnings:
+            echo_warnings_via_pager(graph.warnings)
+            sys.exit(-1)
+
+    # assign edges to subgraphs and report as table
+    prior = neurommsig_knowledge.repository.get_graph()
+    # get node to subgraph map
+    node_to_subgraph = defaultdict(set)
+    for u, v, d in prior.edges(data=True):
+        subgraphs = set(d.get(ANNOTATIONS, {}).get('Subgraph', ()))
+        node_to_subgraph[u].update(subgraphs)
+        node_to_subgraph[v].update(subgraphs)
+
+    prior_subgraphs = set(chain.from_iterable(node_to_subgraph.values()))
+
+    for u, v, k, d in graph.edges(keys=True, data=True):
+        annotations = d.get(ANNOTATIONS)
+        if annotations is None:
+            continue
+        annotations['Subgraph'] = node_to_subgraph[u] | node_to_subgraph[v]
+
+    combine_graph = prior + graph
+    combine_graph.summarize()
+    subgraphs: Mapping[str, BELGraph] = get_subgraphs_by_annotation(combine_graph, 'Subgraph')
+
+    summary_df = pd.DataFrame.from_dict({
+        name: subgraph.summary_dict()
+        for name, subgraph in subgraphs.items()
+        if name in prior_subgraphs
+    }, orient='index')
+    summary_df.to_csv(os.path.join(sheets_repository.output_directory, 'subgraph_summary.tsv'), sep='\t')
 
 
 if __name__ == '__main__':
